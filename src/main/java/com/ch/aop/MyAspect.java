18a8e6df9ffda0541c5c30a55527fdca3150b53c
chenchen <cc.chen.chen@foxmail.com>
2022-03-31 21:42:34 +0800

    GitTest aopTest commit

--- src/main/java/com/ch/aop/MyAspect.java
+++ src/main/java/com/ch/aop/MyAspect.java
@@ -2,14 +2,18 @@ package com.ch.aop;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Component("myAspect")
 @Aspect
 public class MyAspect {
 
-    @Before("execution(* com.ch.aop.Target.*(..))")
+    @Before("pointcut()")
     public void before(){
         System.out.println("前置增强。。");
     }
+
+    @Pointcut("execution(* com.ch.aop.*.*(..))")
+    public void pointcut(){}
 }
